dfaf58ee6c38cfb639111790a7f191ce6cfa5af9
JosefTaticek <jos.taticek@gmail.com>
2024-03-06 09:25:44 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+const App = () => {
+  return <div>
+    <header class="py-4 md:py-6">
+        <div class="container flex items-center justify-between px-4 md:px-6 color:black">
+          <link class="flex items-center space-x-2" href="#">
+            <terminalIcon class="h-6 w-6" />
+            <span class="font-bold tracking-tighter">shadcn</span>
+          </link>
+          <nav class="hidden space-x-4 md:flex">
+            <link
+              className="font-medium text-gray-500 hover:text-gray-900 transition-colors dark:text-gray-400 dark:hover:text-gray-50"
+              href="#"
+            >
+              Features
+            </link>
+            <link
+              className="font-medium text-gray-500 hover:text-gray-900 transition-colors dark:text-gray-400 dark:hover:text-gray-50"
+              href="#"
+            >
+              References
+            </link>
+            <link
+              className="font-medium text-gray-500 hover:text-gray-900 transition-colors dark:text-gray-400 dark:hover:text-gray-50"
+              href="#"
+            >
+              Get Started
+            </link>
+          </nav>
+          <div className="flex items-center space-x-4">
+            <link
+              className="font-medium text-gray-500 hover:text-gray-900 transition-colors dark:text-gray-400 dark:hover:text-gray-50"
+              href="#"
+            >
+              Sign in
+            </link>
+            <link
+              className="inline-block h-10 rounded-md bg-gray-900 px-4 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+              href="#"
+            >
+              Get Started
+            </link>
+          </div>
+        </div>
       </header>
-    </div>
-  );
+
+
+
+
+
+  </div>
 }
 
-export default App;
+export default App
